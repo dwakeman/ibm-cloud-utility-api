768c03f4560ea63acfd441fd47ea26ef55be244f
Dave Wakeman <dwakeman@us.ibm.com>
2019-11-13 22:38:11 -0600

    Initial working version

---
+++ app.js
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2019 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an 'AS IS' BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+const express = require("express");
+var app = express();
+const log4js = require('log4js');
+const appName = require('./package').name;
+const bodyParser = require('body-parser');
+const rcapi = require('./routes/rcapi');
+const logger = log4js.getLogger(appName);
+logger.level ='debug';
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+
+app.get('/',(req,res) => {
+    res.send({"message" : "Yay.. I'm running!  But I am an API so there is nothing to see here!!"});
+});
+
+app.get('/health',(req,res) => {
+    res.send({ "status" : "UP" });
+});
+
+app.get('/instances', rcapi.getResourceInstances);
+app.get('/instances/:instanceid', rcapi.getResourceInstances);
+app.get('/instances/:instanceid/uses', rcapi.getInstanceUses);
+app.get('/instances/:instanceid/aliases', rcapi.getAliasesForInstance);
+//app.post('/encrypt/:keyid', keyprotect.encrypt);
+//app.post('/decrypt/:keyid', keyprotect.decrypt);
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+//    console.log("Listening on http://localhost:" + port);
+    logger.info("Listening on http://localhost:" + port);
+});
---
+++ routes/rcapi.js
@@ -0,0 +1,434 @@
+/**
+ * Copyright 2019 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an 'AS IS' BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+const appName = require('../package').name;
+const https = require('https');
+const querystring = require('querystring');
+const log4js = require('log4js');
+const logger = log4js.getLogger(appName);
+logger.level = 'debug';
+
+/**
+ * Module object that is this module
+ */
+const rcapi = {};
+
+
+/**
+ * Get a key from Key Protect
+ * 
+ * This function is mapped to the '/key/:keyid' route in the API
+ * It will retrieve a key from an instance of Key Protect
+ * 
+ * Method: GET
+ * 
+ * NOTE: This method requires the following environment variables
+ *       KEY_PROTECT_INSTANCE - the GUID of your Key Protect instance
+ *       IBM_API_KEY - a valid API key for a user or service id that has access to the Key Protect instance
+ */
+rcapi.getInstanceUses = async (req, res, next) => {
+
+    // This is the use case where sensitive data is to be wrapped by a root key.  
+    // In this case the DEK is the sensitive data itself
+    
+        // Get parameters from the Request object
+        let instanceId = req.params.instanceid;
+
+        let results ={};
+
+        let path = '/v2/resource_instances/' + instanceId;
+    
+        logger.trace('[getInstanceUses] Entering function.....');
+        logger.debug('[getInstanceUses] Request parameters');
+        logger.debug('[getInstanceUses] Resource Instance ID: ' + instanceId);
+    
+        logger.debug('[getInstanceUses] getting instance data');
+
+        let instance = await callApi(req, path);
+
+        logger.debug('[getInstanceUses] instance crn split is: ' + instance.target_crn.split(":"));
+
+        // get the resource group name in which the alias is located
+        instance.resource_group_name = await callApi(req, '/v2/resource_groups/' + instance.resource_group_id).name;
+
+        logger.debug('[getInstanceUses] Storing instance data');
+        // Store instance data
+        results.instance = instance;
+    
+        logger.debug('[getInstanceUses] getting aliases');
+
+    
+        let aliases = await callApi(req, results.instance.resource_aliases_url);
+
+        let aliasList = [];
+    
+        for (var i=0; i < aliases.resources.length; i++){
+            item = {};
+
+            // create the base alias object
+            alias = aliases.resources[i];
+
+            let resource = {};
+            resource.id = alias.id;
+            resource.guid = alias.guid;
+            resource.url = alias.url;
+            resource.name = alias.name;
+
+//            logger.trace('[getInstanceUses] alias crn split is: ' + alias.target_crn.split(":"));
+
+
+
+            let crn = alias.target_crn.split(":")
+//            logger.trace('[getInstanceUses] binding crn split is: ' + crn[7]);
+
+            // get the name of the target for the alias
+            if (crn[7]) {
+                let targetService = await callApi(req, '/v2/resource_instances/' + crn[7]);
+                logger.trace('[getInstanceUses] target service name is: ' + targetService.name);
+                resource.target_service_name = targetService.name;
+                resource.target_service_url = targetService.url;
+            }
+            
+
+            let resourceGroup = await callApi(req, '/v2/resource_groups/' + alias.resource_group_id);
+            resource.resource_group_name = resourceGroup.name;
+
+            // retrieve and process the bindings
+            let bList = await callApi(req, alias.resource_bindings_url);
+
+            let bindingList = [];
+            // loop through the bindings and get the details for each binding
+            for (var j=0; j < bList.resources.length; j++){
+                let b = bList.resources[j];
+                let binding = {};
+
+                binding.id = b.id;
+                binding.guid = b.guid;
+                binding.url = b.url;
+
+                let bindingResourceGroup = await callApi(req, '/v2/resource_groups/' + b.resource_group_id);
+                binding.resource_group_name = bindingResourceGroup.name;
+
+                // get the name of the target for the binding
+                let bindingCrn = b.target_crn.split(":")
+                if (bindingCrn[7]) {
+                    let bindingTargetService = await callApi(req, '/v2/resource_instances/' + bindingCrn[7]);
+                    logger.trace('[getInstanceUses] target service name is: ' + bindingTargetService.name);
+                    binding.target_service_name = bindingTargetService.name;
+                    binding.target_service_url = bindingTargetService.url
+                }
+
+                bindingList.push(binding);
+            }
+            item.resource = resource;
+            item.bindings = bindingList;
+            aliasList.push(item);
+
+        }
+
+        logger.debug('[getInstanceUses] Storing aliases');
+        // Store inital data
+        results.aliases = aliasList;
+
+
+
+        logger.trace('[getInstanceUses] Exiting function.....');
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify(results));
+        res.end();
+    
+    };
+
+
+
+
+/**
+ * Get a key from Key Protect
+ * 
+ * This function is mapped to the '/key/:keyid' route in the API
+ * It will retrieve a key from an instance of Key Protect
+ * 
+ * Method: GET
+ * 
+ * NOTE: This method requires the following environment variables
+ *       KEY_PROTECT_INSTANCE - the GUID of your Key Protect instance
+ *       IBM_API_KEY - a valid API key for a user or service id that has access to the Key Protect instance
+ */
+rcapi.getResourceInstances = async (req, res, next) => {
+
+// This is the use case where sensitive data is to be wrapped by a root key.  
+// In this case the DEK is the sensitive data itself
+
+    // Get parameters from the Request object
+    let instanceId = req.params.instanceid;
+    let path = instanceId ? '/v2/resource_instances/' + instanceId : '/v2/resource_instances';
+
+    logger.debug('[getResourceInstances] Entering function.....');
+    logger.debug('[getResourceInstances] Request parameters');
+    logger.debug('[getResourceInstances] Resource Instance ID: ' + instanceId);
+
+    
+    let response = await callApi(req, path);
+
+    logger.debug('[getResourceInstances] Exiting function.....');
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify(response));
+    res.end();
+
+};
+
+/**
+ * Get a key from Key Protect
+ * 
+ * This function is mapped to the '/key/:keyid' route in the API
+ * It will retrieve a key from an instance of Key Protect
+ * 
+ * Method: GET
+ * 
+ * NOTE: This method requires the following environment variables
+ *       KEY_PROTECT_INSTANCE - the GUID of your Key Protect instance
+ *       IBM_API_KEY - a valid API key for a user or service id that has access to the Key Protect instance
+ */
+rcapi.getAliasesForInstance = async (req, res, next) => {
+
+    // This is the use case where sensitive data is to be wrapped by a root key.  
+    // In this case the DEK is the sensitive data itself
+    
+        // Get parameters from the Request object
+        let instanceId = req.params.instanceid;
+        let path = '/v2/resource_instances/' + instanceId + '/resource_aliases';
+    
+        logger.debug('[getAliasesForInstance] entering function....')
+        logger.debug('[getAliasesForInstance] Request parameters');
+        logger.debug('[getAliasesForInstance] Resource Instance ID: ' + instanceId);
+    
+        
+        let response = await callApi(req, path);
+    
+        logger.debug('[getAliasesForInstance] Exiting function.....');
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify(response));
+        res.end();
+    
+    };
+
+
+
+
+
+/**
+ * internal function to call the Key Protect API and retrieve a key
+ * 
+ * Method: GET
+ * 
+ * Parameters:
+ *    keyId - the id of the root key to be retrieved
+ * 
+ * NOTE: This method requires the following environment variables
+ *       KEY_PROTECT_INSTANCE - the GUID of your Key Protect instance
+ *       IBM_API_KEY - a valid API key for a user or service id that has access to the Key Protect instance 
+ */
+async function callApi(req, path) {
+
+    logger.trace('[callApi] entering function....');
+
+    let authToken = await getAuthToken(req);
+
+    logger.trace('[callApi] The authentication token is ' + authToken);
+    logger.trace('[callApi] The path is ' + path);
+
+    const headers = {
+        'Accept': 'application/json',
+        'Authorization': authToken
+    }
+
+    const options = {
+        hostname: 'resource-controller.cloud.ibm.com',
+        port: 443,
+        path: path,
+        method: 'GET',
+        headers: headers
+    }
+
+    return new Promise ((resolve, reject) => {
+        const req = https.request(options, (res) =>{
+
+            let rawbody = '';
+
+            res.on('data', d => {
+                rawbody += d;
+            });
+
+            res.on('error', err => {
+                logger.debug('[callApi] exiting with error....');
+                reject(err)
+            });
+
+            res.on('end', () =>{
+                logger.trace('[callApi] exiting with success.... returning ' + rawbody);
+                body = JSON.parse(rawbody);
+                resolve(body)                
+            })
+
+        });
+
+        req.end();
+    });
+
+}; // end of function callApi
+
+
+
+
+/**
+ * Internal function to exchange an IBM Cloud API Key for an IAM oauth token for authentication to 
+ * the Key Protect API
+ * 
+ *  Parameters:
+ *     apikey - the API Key to be used to obtain the oauth token
+ * 
+ *  Returns a JSON object where the oath token is in the 'access_token' field.  It should be used to form
+ *  an Authorization header whose value is 'Bearer <access_token>'
+ * 
+ */
+function getAuthToken(req) {
+
+    logger.trace('[getAuthToken] entering function....');
+    logger.trace('[getAuthToken] Authorization header is ' + req.headers.authorization);
+    return new Promise ((resolve, reject) => {
+
+        if (req.headers.authorization) {
+            logger.trace('[getAuthToken] Found an Authorization header.... will use that.')
+            resolve(req.headers.authorization)
+        }else{
+            logger.trace('[getAuthToken] exchanging API key for auth token ')
+
+            const formData = querystring.stringify({
+                "grant_type": "urn:ibm:params:oauth:grant-type:apikey",
+                "apikey": process.env.IBM_API_KEY
+            });
+        
+            const headers = {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        
+            const options = {
+                hostname: 'iam.cloud.ibm.com',
+                port: 443,
+                path: '/identity/token',
+                method: 'POST',
+                headers: headers
+            }
+
+            const req = https.request(options, (res) => {
+
+                let rawbody = '';
+
+                res.on('data', d => {
+                    rawbody += d;
+                });
+
+                res.on('error', err => {
+                    logger.debug('[getAuthToken] exiting with error....');
+                    reject(err);
+                });
+
+                res.on('end', () =>{
+                    logger.trace('[getAuthToken] exiting with success....');
+                    body = JSON.parse(rawbody);
+                    resolve('Bearer ' + body.access_token);                
+                });
+            })
+        }
+    });
+
+    logger.debug('[getAuthToken] writing form data');
+    req.write(formData);
+    req.end();
+}; //end of function getAuthToken
+
+
+/**
+ * Internal function to validate that the configuration is correct.  If not return an error message. 
+ * 
+ *  Parameters:
+ *     apikey - the API Key to be used to obtain the oauth token
+ * 
+ *  Returns a JSON object where the oath token is in the 'access_token' field.  It should be used to form
+ *  an Authorization header whose value is 'Bearer <access_token>'
+ * 
+ */
+function validateConfig() {
+
+    logger.debug('entering validateConfig....');
+/*
+    const formData = querystring.stringify({
+        "grant_type": "urn:ibm:params:oauth:grant-type:apikey",
+        "apikey": apikey
+    });
+
+    const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    const options = {
+        hostname: 'iam.cloud.ibm.com',
+        port: 443,
+        path: '/identity/token',
+        method: 'POST',
+        headers: headers
+    }
+*/
+    return new Promise ((resolve, reject) => {
+        
+        //Do validations.  Resolve if fine, reject if error.  Return JSON with error messages.
+        //  - KEY_PROTECT_INSTANCE is set
+        //  - IBM_API_KEY is set
+        //  - X-Sensitive-Data header is present and the specified key is in the input.
+ 
+        /*
+        const req = https.request(options, (res) => {
+
+            let rawbody = '';
+
+            res.on('data', d => {
+                rawbody += d;
+            });
+
+            res.on('error', err => {
+                logger.debug('exiting getAuthToken with error....');
+                reject(err)
+            });
+
+            res.on('end', () =>{
+                logger.debug('exiting getAuthToken with success....');
+                body = JSON.parse(rawbody);
+                resolve(body)                
+            })
+
+        });
+
+        logger.debug('In getAuthToken, writing form data');
+        req.write(formData);
+        req.end();
+*/
+    });
+}; //end of function validateConfig
+
+
+
+
+module.exports = rcapi;
